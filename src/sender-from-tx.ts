dfaf624d70c526b4ea8485eb245032af9834a431
Florian Inchingolo <tekiflo@gmail.com>
2023-01-25 19:14:12 +0100

    style: apply eslint

--- src/sender-from-tx.ts
+++ src/sender-from-tx.ts
@@ -1,10 +1,10 @@
 import pkg from '@ethereumjs/tx'
 const { Transaction } = pkg
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 const signedTx = args[0]
 const buf = Buffer.from(signedTx.slice(2), 'hex')
 const tx = Transaction.fromSerializedTx(buf)
-console.log("address: " + tx.getSenderAddress().toString())
-console.log("nonce: " + tx.nonce.toString())
+console.log('address: ' + tx.getSenderAddress().toString())
+console.log('nonce: ' + tx.nonce.toString())
